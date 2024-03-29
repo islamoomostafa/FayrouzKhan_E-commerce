7edb95b7820e5a5bc5413c86fa8b513a78cebdb5
Islam Mostafa <77172741+islamoomostafa@users.noreply.github.com>
2024-03-29 23:59:31 +0200

    Update axiosClient.js

--- Front-End/app/_utils/axiosClient.js
+++ Front-End/app/_utils/axiosClient.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 const apikey = process.env.NEXT_PUBLIC_REST_API_KEY;
-const apiURL = "https://fayrouzkhan.onrender.com/api";
+const apiURL = "https://fayrouzkhan.onrender.com:1337/api";
 const axiosClient = axios.create({
   baseURL: apiURL,
   headers: {
